b9eac5fbe674bc9cc515ef82d0c22f5215418e69
digitaldags <janndaniel.dagusen@gmail.com>
2025-12-02 19:20:27 +0800

    Modify landing page title"

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Wedding RSVP',
+  title: 'Jann and Faith',
   description: 'RSVP for our special day',
 }
 
